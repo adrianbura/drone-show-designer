a3920f100548ef8354d69733f251302e75f9e4a2
adrianbura <124678736+adrianbura@users.noreply.github.com>
2026-08-21 13:00:23 +0300

    Export derived geometry asset audit

--- src/lib/show/diagnostics/index.ts
+++ src/lib/show/diagnostics/index.ts
@@ -14,3 +14,4 @@ export * from "./geometryApplyReadiness";
 export * from "./geometryProposalMaterialisation";
 export * from "./geometryConsequencePresentation";
 export * from "./geometryProposalOpportunity";
+export * from "./geometryDerivedAssetAudit";
